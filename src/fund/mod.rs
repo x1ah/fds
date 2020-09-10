47d2247cb1e835a3e5831345e243048af694067d
x1ah <x1ahgxq@gmail.com>
2020-09-10 10:38:54 +0800

    add detail method

--- src/fund/mod.rs
+++ src/fund/mod.rs
@@ -2,44 +2,77 @@ use std::io::{ErrorKind, Result, Error};
 use url::Url;
 use std::time::SystemTime;
 use reqwest;
+use regex::Regex;
+use serde::{Serialize, Deserialize};
+use serde_json;
 
-#[derive(Debug)]
-pub struct Fund<'a> {
-    pub name: &'a str,
-    pub code: &'a str,
-    pub manager: &'a str,
-    // 净值日期
-    pub v_date: &'a str,
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Fund {
+    pub name: String,
+
+    #[serde(rename(deserialize = "fundcode"))]
+    pub code: String,
+
+    #[serde(default)]
+    pub manager: String,
+
     // 上一个交易日收盘净值
-    pub v_yesterday: f64,
+    #[serde(default, rename(deserialize = "dwjz"))]
+    pub v_yesterday: String,
+
     // 今日估算净值
-    pub v_today: f64,
-    // 今日估算涨跌幅
-    pub v_gap: f64,
+    #[serde(default, rename(deserialize = "gsz"))]
+    pub v_today: String,
+
+    // 今日估算涨跌幅: -1.2 => -1.2%
+    #[serde(default, rename(deserialize = "gszzl"))]
+    pub v_gap: String,
+
     // 估算时间
-    pub v_calc_time: &'a str,
+    #[serde(default, rename(deserialize = "gztime"))]
+    pub v_calc_time: String,
 }
 
-const DETAIL_API: &'static str = "http://fundgz.1234567.com.cn/js/{}.js";
 const SEARCH_API: &'static str = "http://fundsuggest.eastmoney.com/FundSearch/api/FundSearchAPI.ashx";
 
-struct App {}
+pub struct App {}
 
-impl App {
+impl<'a> App {
     pub fn new() -> Self {
         App{}
+    }
+
+    fn gen_code_detail_url(&self, code: String) -> Url {
+        Url::parse(format!("http://fundgz.1234567.com.cn/js/{}.js", code).as_str()).expect("parse detail url error")
     }
 
     pub fn search(&self, query: &str) -> Result<Vec<Fund>> {
         Err(Error::from(ErrorKind::InvalidData))
     }
 
-    pub fn get_detail(&self, code: &str) -> Result<Fund> {
-        let mut url = Url::parse(format!("http://fundgz.1234567.com.cn/js/{}.js", code).as_str()).expect("parse detail url error");
+    pub async fn get_detail(&self, code: String) -> Result<Fund> {
+        let mut url = self.gen_code_detail_url(code);
         let now = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_millis();
         url.query_pairs_mut().append_pair("rt", now.to_string().as_str());
 
-        let _ = reqwest::blocking::get(url).expect("get fund detail error");
-        Err(Error::from(ErrorKind::NotFound))
+        let text = reqwest::get(url).await.expect("error").text().await.expect("parse error");
+        self.to_fund(text).await
+    }
+
+    async fn to_fund(&self, text: String) -> Result<Fund> {
+        let pattern = match Regex::new(r"jsonpgz\((?P<data>.+)\)") {
+            Ok(v) => v,
+            Err(e) => return Err(Error::new(ErrorKind::InvalidData, e.to_string())),
+        };
+        let res = pattern.captures(&text);
+
+        let json_data = match res {
+            Some(v) => v.name("data").expect("err").as_str(),
+            _ => return Err(Error::from(ErrorKind::InvalidData)),
+        };
+
+        let mut f: Fund = serde_json::from_str(json_data)?;
+        f.v_gap += "%";
+        Ok(f)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,15 @@
 mod fund;
-use fund::Fund;
 use std::time::SystemTime;
 use std::io::Result;
+use tokio;
 
-fn main() -> Result<()> {
+#[tokio::main]
+async fn main() -> Result<()> {
     println!("Hello, world!");
-    let f = Fund{
-        name: "大大",
-        code: "12123",
-        manager: "带带",
-        v_date: "",
-        v_yesterday: 0.0,
-        v_today: 0.0,
-        v_gap: 0.0,
-        v_calc_time: ""
-    };
-    println!("fund: {:?}", f);
     let now = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_millis();
     println!("now: {}", now);
+
+    let resp = fund::App::new().get_detail(String::from("519983")).await.unwrap();
+    println!("{:?}", resp);
     Ok(())
 }
