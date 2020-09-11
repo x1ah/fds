bfb9df0baada5ef317ddbf0acc7104f6625eec44
x1ah <x1ahgxq@gmail.com>
2020-09-11 17:20:48 +0800

    change crate name

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,7 @@ use std::io::Result;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let resp = fund::App::new()
-        .get_detail("003494")
-        .await
-        .unwrap();
+    let resp = fund::App::new().get_detail("003494").await.unwrap();
     println!("{:?}", resp);
 
     let v = fund::App::new().search("广发纳斯达克").await;
